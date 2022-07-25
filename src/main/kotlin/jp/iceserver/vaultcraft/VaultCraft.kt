74e0a485fab7b20b4547f07f942cc718d3078787
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-07-25 20:37:08 +0900

    [Add] 存在するアカウントをすべて読み込み

--- src/main/kotlin/jp/iceserver/vaultcraft/VaultCraft.kt
+++ src/main/kotlin/jp/iceserver/vaultcraft/VaultCraft.kt
@@ -2,17 +2,16 @@ package jp.iceserver.vaultcraft
 
 import hazae41.minecraft.kutils.bukkit.init
 import hazae41.minecraft.kutils.bukkit.listen
+import jp.iceserver.vaultcraft.account.Account
 import jp.iceserver.vaultcraft.commands.*
 import jp.iceserver.vaultcraft.config.MainConfig
 import jp.iceserver.vaultcraft.economy.VaultEconomy
 import jp.iceserver.vaultcraft.tables.*
 import net.milkbowl.vault.economy.Economy
+import org.bukkit.Bukkit
 import org.bukkit.event.server.PluginEnableEvent
 import org.bukkit.plugin.ServicePriority
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
@@ -25,6 +24,8 @@ class VaultCraft : AbstractVaultCraft()
         lateinit var plugin: VaultCraft
         lateinit var economy: Economy
     }
+
+    val accounts: MutableList<Account> = mutableListOf()
 
     override fun onEnable()
     {
@@ -48,6 +49,10 @@ class VaultCraft : AbstractVaultCraft()
             SchemaUtils.create(
                 Accounts
             )
+
+            Accounts.selectAll().forEach {
+                accounts.add(Account(it[Accounts.id], Bukkit.getOfflinePlayer(it[Accounts.uniqueId]), it[Accounts.balance]))
+            }
         }
 
         if (!(server.pluginManager.getPlugin("Vault") ?: return).isEnabled)
