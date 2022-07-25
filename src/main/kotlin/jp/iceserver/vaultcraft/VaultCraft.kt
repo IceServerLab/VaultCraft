af96615b0266c268d758119c92a69083bf7c4a88
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-07-25 19:25:59 +0900

    [Add] Vaultをフック！！捕まえたぞ！！

--- src/main/kotlin/jp/iceserver/vaultcraft/VaultCraft.kt
+++ src/main/kotlin/jp/iceserver/vaultcraft/VaultCraft.kt
@@ -1,14 +1,38 @@
 package jp.iceserver.vaultcraft
 
+import hazae41.minecraft.kutils.bukkit.listen
+import jp.iceserver.vaultcraft.economy.VaultEconomy
+import net.milkbowl.vault.economy.Economy
+import org.bukkit.event.server.PluginEnableEvent
+import org.bukkit.plugin.ServicePriority
+
 class VaultCraft : AbstractVaultCraft()
 {
     companion object
     {
         lateinit var plugin: VaultCraft
+        lateinit var economy: Economy
     }
 
     override fun onEnable()
     {
         plugin = this
+
+        if (!(server.pluginManager.getPlugin("Vault") ?: return).isEnabled)
+        {
+            listen<PluginEnableEvent> {
+                if (it.plugin.name != "Vault") return@listen
+
+                hookVault()
+                PluginEnableEvent.getHandlerList().unregister(this)
+            }
+        }
+        else hookVault()
+    }
+
+    private fun hookVault()
+    {
+        economy = VaultEconomy()
+        server.servicesManager.register(Economy::class.java, economy, this, ServicePriority.Normal)
     }
 }
